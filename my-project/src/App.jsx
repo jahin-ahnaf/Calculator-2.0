774d70623021cf4c45400a10a0f5eb3222a6b5f8
Jahin Ahnaf <jahinahnaf16@gmail.com>
2024-09-05 21:08:03 +0600

    Monitor Done!

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,13 @@ function App() {
 
   return (
     <>
-      <h1 className="bg-red-400 text-white">Hello World</h1>
+    <div className="flex content-center justify-center items-center absolute top-0 left-0 right-0 bottom-0">
+      <div className="justify-center flex bg-zinc-800 h-96 w-80 rounded-2xl">
+          <div className="bg-lime-200 w-full h-28 rounded-xl m-2">
+            <input disabled placeholder="_" className="outline-none bg-lime-200 font-extrabold text-2xl placeholder-lime-600 w-full rounded-xl p-5 h-full text-lime-600"></input>
+          </div>
+      </div>
+    </div>
     </>
   )
 }
